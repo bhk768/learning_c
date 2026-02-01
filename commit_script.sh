4787b8ad161fe309b520589fc24dddcc90294c06
Mahiman Singh <74301608+bhk768@users.noreply.github.com>
2026-02-01 06:00:32 +0000

    Day commit 1 on 2026-02-01T06:00:32

--- commit_script.sh
+++ commit_script.sh
@@ -1,15 +1,17 @@
-for d in $(seq 0 99); do
-  DATE=$(date -d "2025-08-26 +$d day" +"%Y-%m-%dT06:00:32")
+# Loop from Feb 1, 2026 up to May 5, 2026
+for d in $(seq 0 $(($(($(date -d "2026-05-05" +%s) - $(date -d "2026-02-01" +%s))) / 86400)))
+do
+  DATE=$(date -d "2026-02-01 +$d day" +"%Y-%m-%dT06:00:32")
 
   # FIRST COMMIT
   echo "Commit 1 for $DATE" >> progress.log
   GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git add .
   GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git commit -m "Day commit 1 on $DATE"
 
-  # SECOND COMMIT (same date, different time)
+  # SECOND COMMIT (same date, +1 minute)
   DATE2=$(date -d "$DATE +1 minute" +"%Y-%m-%dT06:01:32")
   echo "Commit 2 for $DATE2" >> progress.log
   GIT_AUTHOR_DATE="$DATE2" GIT_COMMITTER_DATE="$DATE2" git add .
   GIT_AUTHOR_DATE="$DATE2" GIT_COMMITTER_DATE="$DATE2" git commit -m "Day commit 2 on $DATE2"
 
-done && git push
+done
