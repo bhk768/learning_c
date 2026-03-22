b36be8759ace76b38dc2203e1d2a4bf96ead8fdd
Mahiman Singh <74301608+bhk768@users.noreply.github.com>
2026-03-22 06:00:32 +0000

    Day commit 1 on 2026-03-22T06:00:32

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
-for d in $(seq 0 15); do
-  DATE=$(date -d "2026-03-01 +$d day" +"%Y-%m-%dT06:00:32")
-
+# Array of specific dates
+for DATE in "2026-03-22T06:00:32" "2026-04-14T06:00:32" "2026-04-18T06:00:32"
+do
   # FIRST COMMIT
   echo "Commit 1 for $DATE" >> progress.log
   GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git add .
   GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git commit -m "Day commit 1 on $DATE"
 
-  # SECOND COMMIT (same date, different time)
+  # SECOND COMMIT (same date, +1 minute)
   DATE2=$(date -d "$DATE +1 minute" +"%Y-%m-%dT06:01:32")
   echo "Commit 2 for $DATE2" >> progress.log
   GIT_AUTHOR_DATE="$DATE2" GIT_COMMITTER_DATE="$DATE2" git add .
   GIT_AUTHOR_DATE="$DATE2" GIT_COMMITTER_DATE="$DATE2" git commit -m "Day commit 2 on $DATE2"
 
-done && git push
+done
