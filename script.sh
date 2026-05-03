cbeeed7fbac69fc30885a6b95c5cb63d79fa7ce2
Mahiman Singh <74301608+bhk768@users.noreply.github.com>
2026-05-03 19:03:36 +0000

    Save work before rebase

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
-# Loop from Feb 1, 2026 to May 5, 2026
+#!/bin/bash
+
 START="2026-02-01"
 END="2026-05-05"
 
@@ -7,16 +8,19 @@ while [ "$(date -d "$current" +%Y-%m-%d)" != "$(date -d "$END +1 day" +%Y-%m-%d)
   DATE=$(date -d "$current" +"%Y-%m-%dT06:00:32")
 
   # FIRST COMMIT
-  echo "Commit 1 for $DATE" >> progress.log
-  GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git add .
+  echo "Commit 1 for $DATE" > progress.log
+  git add progress.log
   GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git commit -m "Day commit 1 on $DATE"
 
   # SECOND COMMIT (+1 minute)
   DATE2=$(date -d "$DATE +1 minute" +"%Y-%m-%dT06:01:32")
-  echo "Commit 2 for $DATE2" >> progress.log
-  GIT_AUTHOR_DATE="$DATE2" GIT_COMMITTER_DATE="$DATE2" git add .
+  echo "Commit 2 for $DATE2" > progress.log
+  git add progress.log
   GIT_AUTHOR_DATE="$DATE2" GIT_COMMITTER_DATE="$DATE2" git commit -m "Day commit 2 on $DATE2"
 
-  # Move to next day
+  # Next day
   current=$(date -d "$current +1 day" +%Y-%m-%d)
 done
+
+git push
+
